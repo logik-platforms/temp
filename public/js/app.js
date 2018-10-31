d79787fc8924341d45c82067d37ab0fdc2acc047
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-10-31 15:21:08 -0400

    Completed logo

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,40 @@
 $(document).ready(() => {
+    // initial logo scaling
     $("#logo").effect("scale", {
         percent: 500
     }, 5 * 1000, () => {
-        console.log("Complete animation");
-    })
+        $("#logo").fadeOut("fast", () => {
+            let angle = 0;
+            $("#fadeInLogo").fadeIn("fast");
+            $("#fadeInLogo").css("display", "flex");
+            setInterval(() => {
+                angle += 1;
+                $("#inlineLogo").rotate(angle);
+            }, 50);
+            randomTimeout();
+        });
+    });
+
+    function distort(element) {
+        element.addClass("distort")
+        setTimeout(() => {
+            element.removeClass("distort");
+            if (Math.random() <= 0.5) {
+                setTimeout(() => {
+                    element.addClass("distort");
+                    setTimeout(() => {
+                        element.removeClass("distort");
+                    }, 0.1 * 1000);
+                }, 0.05 * 1000);
+            }
+        }, 0.1 * 1000);
+    }
+
+    function randomTimeout() {
+        let interval = setTimeout(() => {
+            distort($("#titleText"));
+            clearTimeout(interval);
+            randomTimeout();
+        }, (Math.random() * 7 + 2) * 1000);
+    }
 });
