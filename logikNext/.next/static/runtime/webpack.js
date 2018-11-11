8bd72a23d626794a6734522a1efde16d9388b0e2
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-11-10 20:19:25 -0500

    Created git ignore

--- logikNext/.next/static/runtime/webpack.js
+++ logikNext/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cd2563ef7018715227c7";
+/******/ 	var hotCurrentHash = "ba6ee08c2e6105647ddd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
